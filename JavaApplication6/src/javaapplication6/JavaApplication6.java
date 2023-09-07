7708bdbe6d2cb89a8a393d41d95ef159359864f7
MaximilianoFC <aaronmexes@gmail.com>
2023-09-07 12:38:45 -0300

    miembros del grupo

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+miembros del grupo:
+Mario Cesar Sepulveda
+Maximiliano Colin
+Melany Gorosito
+ivana mariel cabrera
+Franco Uriel Sanchez
  */
 package javaapplication6;
 
