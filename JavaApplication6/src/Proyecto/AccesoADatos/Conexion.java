ff9532d4e1a8a4d50a9a88ad79a9bf1a533ebc95
MaximilianoFC <aaronmexes@gmail.com>
2023-09-08 10:50:44 -0300

    carga de drivers

---
+++ JavaApplication6/src/Proyecto/AccesoADatos/Conexion.java
@@ -0,0 +1,13 @@
+package Proyecto.AccesoADatos;
+
+
+public class Conexion {
+    private static final String URL="jdbc:mysql://localhost/";
+    private static final String DB="universidadulp";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    
+    
+
+
+}
