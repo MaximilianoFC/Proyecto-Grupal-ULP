5b3343a07008d726c380826ebaf95ba369f21d62
MaximilianoFC <aaronmexes@gmail.com>
2023-09-08 11:10:54 -0300

    conexion a base de datos

--- JavaApplication6/src/Proyecto/AccesoADatos/Conexion.java
+++ JavaApplication6/src/Proyecto/AccesoADatos/Conexion.java
@@ -1,13 +1,45 @@
 package Proyecto.AccesoADatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 
 public class Conexion {
     private static final String URL="jdbc:mysql://localhost/";
-    private static final String DB="universidadulp";
+    private static final String DB="proyectogrupaluniversidad";
     private static final String USUARIO="root";
     private static final String PASSWORD="";
     
+    private static connection connection;
     
+private Conexion(){}
 
+public static Connection getConexion() throws ClassNotFoundException{
+    
+    if(connection ==null){
+    }
+        try{
+        Class.forName("org.mariadb.jdbc.Driver");
+        connection = DriverManager
+                .getConnection(URL+DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                +"&user=" + USUARIO+"&password="+PASSWORD);
+        
+     
+        }catch(SQLException ex){
+        JOptionPane.showMessageDialog(null,"Error al conectarse a la BD");
+                       {
+                JOptionPane.showMessageDialog(null,"Error al cargar los Driver");
+                
+                }
+        
+    
+    
+    }
+
+        return connection;
+}
+    
 
 }
