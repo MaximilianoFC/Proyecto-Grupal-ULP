779647afa42fc963f61f36737673a0ceb0112cf7
Melany2023 <gorositomelany9@gmail.com>
2023-09-14 10:51:41 -0300

    Cree clase MateriaData

---
+++ JavaApplication6/src/Proyecto/AccesoADatos/MateriaData.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto.AccesoADatos;
+
+import Entidades.Materia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
+
+public class MateriaData {
+    
+    private Connection con=null;
+    
+    public MateriaData(){
+        con=Conexion.getConexion();
+    }
+    
+    
+    public void guardarMateria(Materia materia){
+        String sql="INSERT INTO materia (nombre, año, estado)"
+                +"VALUES (?, ?, ?)";
+        
+        try {
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        
+        ps.setString(1, materia.getNombre());
+        ps.setInt(2, materia.getAnioMateria());
+        ps.setBoolean(3, materia.isActivo());
+        ps.executeUpdate();
+        
+        ResultSet rs=ps.getGeneratedKeys();
+        if(rs.next()){
+            materia.setIdMateria(rs.getInt(1));
+            JOptionPane.showMessageDialog(null, "Materia guardada");
+        }
+        
+        ps.close();
+        
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");
+        }
+    }
+    
+    
+    public void modificarMateria(Materia materia){
+        String sql="UPDATE materia SET nombre = ?, año = ?"
+                + "WHERE idMateria = ?";
+        
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnioMateria());
+            ps.setInt(3, materia.getIdMateria());
+            int exito = ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null, "Materia modificada");
+            }
+            
+            
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");
+        }
+    }
+}
+
